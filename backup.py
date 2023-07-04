a90de298033cc62fee163aed81dedf865642a491
Anonymous_monkey <Anonymous@gmail.com>
2023-07-04 18:24:14 +0530

    New backup feature

---
+++ backup.py
@@ -0,0 +1,40 @@
+import os,zipfile,voice,datetime,send2trash
+
+class Backup:
+    def __init__(self):
+        self.voice = voice.Voice()
+        
+    def backup(self,folder):
+        folder = os.path.abspath(folder) # Checking if it's an absolute path
+
+        number = 1
+        date = datetime.datetime.now()
+        while True:
+            zipFileName = os.path.basename(folder) + "_"+f"{date.day}_{date.month}_{date.year}"+".zip"
+            if os.path.exists(zipFileName):
+                
+                break
+            number +=1
+            self.voice.speak(f"Creating backup file of the folder {os.path.basename(folder)}")
+            backup = zipfile.ZipFile(zipFileName,'w') # Create the Zip file
+
+            for folders,subfolders,filenames in os.walk(folder):
+                print(f"Backing up {folders} ...")
+
+                backup.write(folders)
+
+                for file in filenames:
+                    already= os.path.basename(folder) + "_"
+                    if file.startswith(already) and file.endswith('.zip'):
+                        continue
+                    backup.write(os.path.join(folders,file))
+
+            backup.close()
+
+
+
+
+
+
+back = Backup()
+back= back.backup('D:\\Python\\Friday')
